cb6d066e0e8671643a659e4ce3fdd3bb35739ed3
Kostiantyn Gavura <gavura@duck.com>
2022-04-02 19:30:26 +0200

    add pojo class User

---
+++ src/main/java/org/gavura/entity/User.java
@@ -0,0 +1,23 @@
+package org.gavura.entity;
+
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+
+@Builder
+@Getter
+@ToString
+@EqualsAndHashCode
+public class User {
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
+    private Long id;
+    private String username;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private String phone;
+    private Integer userStatus;
+}
