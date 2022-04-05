c73edd6e99b0da8de18418f9e9405bde8f025cb3
Kostiantyn Gavura <gavura@duck.com>
2022-04-05 18:52:49 +0200

    add annotations to tag class

--- src/main/java/org/gavura/entity/Tag.java
+++ src/main/java/org/gavura/entity/Tag.java
@@ -1,9 +1,13 @@
 package org.gavura.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 @Data
+@AllArgsConstructor
 public class Tag {
     private int id;
+    @EqualsAndHashCode.Include
     private String name;
 }
