78c4b42763ccdeb0d1c4e3e7fbb1986d4dd283e1
Kostiantyn Gavura <gavura@duck.com>
2022-04-09 19:46:06 +0200

    add STORE_ORDER_BY_ID

--- src/main/java/org/gavura/uritemplate/StoreServiceUri.java
+++ src/main/java/org/gavura/uritemplate/StoreServiceUri.java
@@ -6,4 +6,5 @@ import lombok.experimental.UtilityClass;
 public class StoreServiceUri {
     public static final UriTemplate STORE_INVENTORY = new UriTemplate("store/inventory");
     public static final UriTemplate STORE_ORDER = new UriTemplate("store/order");
+    public static final UriTemplate STORE_ORDER_BY_ID = new UriTemplate("store/order/%s");
 }
