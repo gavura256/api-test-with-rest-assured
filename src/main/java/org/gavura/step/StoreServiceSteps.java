009020066c125c98e0494246b2c4ec9cc34a620c
Kostiantyn Gavura <gavura@duck.com>
2022-04-09 13:49:01 +0200

    add postStore method

--- src/main/java/org/gavura/step/StoreServiceSteps.java
+++ src/main/java/org/gavura/step/StoreServiceSteps.java
@@ -1,17 +1,23 @@
 package org.gavura.step;
 
 import lombok.experimental.UtilityClass;
+import org.gavura.entity.Store;
 import org.gavura.service.StoreService;
 
 import static org.gavura.uritemplate.StoreServiceUri.STORE_INVENTORY;
+import static org.gavura.uritemplate.StoreServiceUri.STORE_ORDER;
 
 
 @UtilityClass
 public class StoreServiceSteps {
     private static final StoreService STORE_SERVICE = StoreService.getInstance();
 
-
     public static String getInventory() {
         return STORE_SERVICE.getRequest(STORE_INVENTORY).getBody().asPrettyString();
     }
+
+    public static String postStore(Store storeOrder){
+        return STORE_SERVICE.postRequest(STORE_ORDER, storeOrder).getBody().asPrettyString();
+    }
+
 }
